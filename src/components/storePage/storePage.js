beef3574e7d901cc2d5503de63b242680ba63554
Izhanitov <dm.define@gmail.com>
2022-11-15 19:54:45 +0700

    StorePage refactoried to func component

--- src/components/storePage/storePage.js
+++ src/components/storePage/storePage.js
@@ -1,82 +1,75 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import {NavLink} from 'react-router-dom';
 
 import RestService from '../../services/restService';
 
-export default class StorePage extends Component {
-    restService = new RestService();
-    state = {
-        categories: null
-    }
+const StorePage = () => {
+    const restService = new RestService();
 
-    componentDidMount() {
-        this.updateData();
-    }
+    const [categories, setCategories] = useState({data: null});
+    const [checkLoad, setCheckLoad] = useState(false);
 
-    onCategoriesLoaded = (categories) => {               
-        this.setState({categories}); 
-    }
+    useEffect(() => {
+        updateData();
+    }, []);
 
-    updateData = () => {
-        this.restService.getAllTasks()
-            .then(this.onCategoriesLoaded);
+    const updateData = () => {
+        restService.getAllTasks()
+        .then(categories => {
+            setCategories({data: categories})
+            setCheckLoad(true);        
+        });
     }
     
-    renderItem = (item) => {
+    const renderItem = (item) => {
         
-            const { categoryId, categoryName, lowPrice } = item;
+        const { categoryId, categoryName, lowPrice } = item;
 
-            const src = "./img/categories/cat" + categoryId + ".png";
+        const src = `./img/categories/cat${categoryId}.png`;
 
-            const url = "/category/" + categoryId;
+        const url = `/category/${categoryId}`;
 
-            return (
-                    <div key={categoryId} className='col product row'>
-                        <div className='col-6 product-image'>
-                            <img alt={categoryName} className='image-p' src={src}></img>
-                        </div>
-                        <div className='col-6 product-name'> 
-                            <h3>{categoryName}</h3>
-                        </div>
-                        <div className='row prod-info-block'>
-                            <div className='col-6 prod-info'>
-                                цена от {lowPrice} руб
-                            </div>
-                            <div className='col-6 prod-info'>
-                                <NavLink className='info-link' to={url}>ПОДРОБНЕЕ</NavLink>
-                            </div> 
-                        </div>                      
+        return (
+                <div key={categoryId} className='col product row'>
+                    <div className='col-6 product-image'>
+                        <img alt={categoryName} className='image-p' src={src}></img>
                     </div>
-            )     
-            
-            
-    }
-   
+                    <div className='col-6 product-name'> 
+                        <h3>{categoryName}</h3>
+                    </div>
+                    <div className='row prod-info-block'>
+                        <div className='col-6 prod-info'>
+                            цена от {lowPrice} руб
+                        </div>
+                        <div className='col-6 prod-info'>
+                            <NavLink className='info-link' to={url}>ПОДРОБНЕЕ</NavLink>
+                        </div> 
+                    </div>                      
+                </div>
+        ) 
+    }  
 
-    render() {
-
-        const { categories } = this.state;
-
-        if (!categories) {
-            return <h1>Загрузка</h1>
-        }     
-
+    if (!checkLoad) {
+        return <h1>Загрузка</h1>
+    }  else {
         return(
             <div className='container'>
-                {categories.$values.map((item, index) => 
+                {categories.data.$values.map((item, index) => 
                     index % 2 === 1 ? null : (
                         <div className='row la'>
-                           {
-                                this.renderItem(item)
-                           }
-                           {
-                                index + 1 < categories.$values.length ? 
-                                this.renderItem(categories.$values[index + 1]) : null
+                            {
+                                renderItem(item)
+                            }
+                            {
+                                index + 1 < categories.data.$values.length ? 
+                                renderItem(categories.data.$values[index + 1]) : null
                             }
                         </div> 
                     )                    
                 )}            
             </div>
         );
-    }
+    } 
 }
+
+export default StorePage;
