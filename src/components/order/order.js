32ae233dea34434e6812f9ae569ac94386625baf
Izhanitov <dm.define@gmail.com>
2022-11-16 22:26:37 +0700

    Order modified

--- src/components/order/order.js
+++ src/components/order/order.js
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useMemo, useState, useCallback } from "react";
 import { useStore, useDispatch } from "react-redux";
 import { cleanProducts } from "../../actions";
 import useValidateInput from "../useValidateInput/useValidateInput";
@@ -6,6 +6,9 @@ import useValidateInput from "../useValidateInput/useValidateInput";
 import RestService from "../../services/restService";
 
 const Order = () => {  
+
+    const [checkValidInputs, setCheckValidInputs] = useState(false);
+    const [beforeConfirm, setBeforeConfirm] = useState(true);
 
     const phone = useValidateInput("", "phone");
     const email = useValidateInput("", "email");
@@ -15,23 +18,46 @@ const Order = () => {
     const city = useValidateInput("", "city");
     const building = useValidateInput("", "building");
 
-    const dispatch = useDispatch();
-    
-    const store = useStore();
-    const products = useMemo(() => store.getState().products, [store]);
+    const allFields = useMemo(() => [phone, email, name, surname, region, city, building], [phone, email, name, surname, region, city, building]);
 
-    const checkContacts = () => {
-        //return 
-    }
+    const dispatch = useDispatch();
+    const restService = new RestService();
+
+    const store = useStore();
+    const products = useMemo(() => store.getState().products, [store]);    
+
+    const checkContacts = useCallback(() => {
+        const check = allFields.every(field => field.checkInput)
+        setCheckValidInputs(check);
+        setBeforeConfirm(false);
+        return check;
+    }, [allFields]);
 
     const cleanStore = () => {
         dispatch(cleanProducts());
     }
 
-    const confirmOrder = () => {
-        checkContacts() ? console.log("Ошибок нет") : console.log("Ошибки");
+    const sendData = () => {
+        const bundle = {
+            name: name.value,
+            surname: surname.value,
+            region: region.value, 
+            city: city.value, 
+            building: building.value, 
+            phone: phone.value, 
+            email: email.value, 
+            products
+        }
+        
+        restService.sendOrder(bundle)
+                    .then(res => res === 200 ? cleanStore() : null)
+                    .error(err => console.log(err));
+    }
 
-    }  
+    const confirmOrder = () => {
+        if(checkContacts())
+            sendData(); 
+    };
 
     const renderInputError = (type) => {
         return type.errorMessage === "" ? null : <span>{type.errorMessage}</span>
@@ -40,29 +66,33 @@ const Order = () => {
     return (
         <div className="container">
             <div className="row">
-                <h4>Имя</h4>
-                <input onChange={name.onChange} value={name.value} style={{borderColor: name.errorMessage ? "Red" : "initial"}}></input>
-                {renderInputError(name)}
-                <h4>Фамилия</h4>
-                <input onChange={surname.onChange} value={surname.value}></input>
-                {renderInputError(surname)}
-                <h4>Телефон</h4>
-                <input onChange={phone.onChange} value={phone.value}></input>
-                {renderInputError(phone)}
-                <h4>Email</h4>
-                <input onChange={email.onChange} value={email.value}></input>
-                {renderInputError(email)}
-                <h4>Регион</h4>
-                <input onChange={region.onChange} value={region.value}></input>
-                {renderInputError(region)}
-                <h4>Город</h4>
-                <input onChange={city.onChange} value={city.value}></input>
-                {renderInputError(city)}
-                <h4>Дом</h4>
-                <input onChange={building.onChange} value={building.value}></input>
-                {renderInputError(building)}
-
+                <div className="col-7">
+                    <h4>Имя</h4>
+                    <input onChange={name.onChange} value={name.value} style={{borderColor: name.errorMessage ? "Red" : "initial"}}></input>
+                    {renderInputError(name)}
+                    <h4>Фамилия</h4>
+                    <input onChange={surname.onChange} value={surname.value}></input>
+                    {renderInputError(surname)}
+                    <h4>Телефон</h4>
+                    <input onChange={phone.onChange} value={phone.value}></input>
+                    {renderInputError(phone)}
+                    <h4>Email</h4>
+                    <input onChange={email.onChange} value={email.value}></input>
+                    {renderInputError(email)}
+                    <h4>Регион</h4>
+                    <input onChange={region.onChange} value={region.value}></input>
+                    {renderInputError(region)}
+                    <h4>Город</h4>
+                    <input onChange={city.onChange} value={city.value}></input>
+                    {renderInputError(city)}
+                    <h4>Дом</h4>
+                    <input onChange={building.onChange} value={building.value}></input>
+                    {renderInputError(building)}                    
+                </div>
+            </div>
+            <div>                
                 <button onClick={() => confirmOrder()}>Отправить</button>
+                {checkValidInputs || beforeConfirm ? null : <span>Все поля должны быть заполнены!</span>} 
             </div>
         </div>
     )
