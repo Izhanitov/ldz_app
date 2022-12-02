01b7bb535b710b0e8b590720773b8633a28d2870
Izhanitov <dm.define@gmail.com>
2022-12-02 20:10:19 +0700

    bootstrap added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,40 +1,37 @@
-import { Component } from 'react';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Component } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Header from "../header/header.js";
+import Menu from "../menu/menu";
+import Mainsection from "../mainsection/mainsection";
+import Factory from "../factory/factory";
+import StorePage from "../storePage/storePage.js";
+import Category from "../category/category.js";
+import ShoppingBasket from "../shoppingBasket/shoppingBasket.js";
+import Order from "../order/order.js";
 
-import Header from '../header/header.js';
-import Menu from '../menu/menu';
-import Mainsection from '../mainsection/mainsection';
-import Factory from '../factory/factory';
-import StorePage from '../storePage/storePage.js';
-import Category from '../category/category.js';
-import ShoppingBasket from '../shoppingBasket/shoppingBasket.js';
-import Order from '../order/order.js';
-
-import '../styles/bootstrap-grid.min.css'; 
-import '../styles/styles.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles/styles.css";
 
 export default class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div className="mainclass">
-                    <Header />
-                    <Menu />
-                    <main>
-                        <Routes>
-                            <Route path="/factory" element={<Factory/>} />
-                            <Route path="/" element={<Mainsection/>} />
-                            <Route path="/store" element={<StorePage/>} />
-                            <Route path="/category/:id" element={<Category/>} />
-                            <Route path="/basket" element={<ShoppingBasket/>} />
-                            <Route path="/order" element={<Order/>} />
-                        </Routes>  
-                    </main>         
-                </div>
-            </Router>
-        );
-    }
+  render() {
+    return (
+      <Router>
+        <div className="mainclass">
+          <Header />
+          <Menu />
+          <main>
+            <Routes>
+              <Route path="/factory" element={<Factory />} />
+              <Route path="/" element={<Mainsection />} />
+              <Route path="/store" element={<StorePage />} />
+              <Route path="/category/:id" element={<Category />} />
+              <Route path="/basket" element={<ShoppingBasket />} />
+              <Route path="/order" element={<Order />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    );
+  }
 }
-
